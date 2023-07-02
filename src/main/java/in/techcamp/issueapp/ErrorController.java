42ede68801de8bd238c4df376531a7cc37f96f6b
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-07-02 19:25:00 +0900

    Create ErrorController.java

---
+++ src/main/java/in/techcamp/issueapp/ErrorController.java
@@ -0,0 +1,18 @@
+//package in.techcamp.issueapp;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class ErrorController implements org.springframework.web.servlet.error.ErrorController{
+//    @RequestMapping("/error")
+//    public String handleError() {
+//        // カスタムのエラーページのテンプレート名を返す
+//        return "error";
+//    }
+//
+//    @Override
+//    public String getErrorPath() {
+//        return "/error";
+//    }
+//}
